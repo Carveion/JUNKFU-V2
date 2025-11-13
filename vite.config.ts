21d48ca4f819ef80db0298abf086943a26155c0b
Carveion <karthikeyanelambharathi@gmail.com>
2025-11-13 18:28:20 +0530

    Add allowedHosts to preview configuration

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      preview: {
+        allowedHosts: ['junkfu-v2.onrender.com']
       }
     };
 });
